85eceb26284c0f3ea73cf71a26fd13fe5936d6e0
Patricio Sanchez Vuichard <patriciosanchezvuichard@gmail.com>
2023-02-14 16:36:31 +0100

    iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,27 @@
 const express = require("express");
-const router = require("express").Router();
+const router = express.Router();
+const Celebrity = require("../models/Celebrity.model.js")
 
 //rutas
+// GET "/celebrities/create"
+router.get("/create",(req, res, next)=>{
+  res.render("celebrities/new-celebrity.hbs")
+})
+
+// POST "/celebrities/create"
+router.post("/create", async (req, res, next)=>{
+  const { name, occupation, catchPhrase } = req.body
+  try {
+    await Celebrity.create({
+      name,
+      occupation,
+      catchPhrase
+    })
+    console.log(req.body)
+    res.redirect("/")
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router;
